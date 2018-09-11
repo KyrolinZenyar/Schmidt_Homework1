734ac6aa9c2c1ab73f2a0c0df68fcef3faec63c3
Daniel Schmidt <schmidt.152@wright.edu>
2018-09-10 23:11:16 -0400

    Beginnings of a Drawing Panel
    Found several resources that allowed me to piece together how to build
    the skeleton of the drawing panel - here be poorly documented Microsoft
    features in use.

--- Schmidt_Homework1/Schmidt_Homework1/DrawingPanel.xaml.cs
+++ Schmidt_Homework1/Schmidt_Homework1/DrawingPanel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,70 @@ namespace Schmidt_Homework1
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DrawingPanel : ContentPage
 	{
-		public DrawingPanel ()
+        private Dictionary<long, SKPath> tempPath = new Dictionary<long, SKPath>();
+        private List<SKPath> paths = new List<SKPath>();
+        private SKColor color = new SKColor(0, 0, 0);
+        private int strokeWidth = 3;
+
+        public DrawingPanel ()
 		{
 			InitializeComponent ();
 		}
+
+        private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var paintSurface = e.Surface;
+            var paintCanvas = paintSurface.Canvas;
+            //Still need to add clear button and save button
+            paintCanvas.Clear(SKColors.White);
+
+            //Define SKPaint here (need to make colorpicker and strokeWidth picker)
+            var touchPathStroke = new SKPaint
+            {
+                Color = color,
+                Style = SKPaintStyle.Stroke,
+                IsAntialias = true,
+                StrokeWidth = strokeWidth
+            };
+
+            //As new paths (temporary or permanent) get created, draw them on the canvas
+            foreach (var path in tempPath)
+            {
+                paintCanvas.DrawPath(path.Value, touchPathStroke);
+            }
+            foreach (var path in paths)
+            {
+                paintCanvas.DrawPath(path, touchPathStroke);
+            }
+
+        }
+
+        private void OnTouch(object sender, SKTouchEventArgs e)
+        {
+
+            if (e.ActionType == SKTouchAction.Pressed)
+            {
+                //When a line begins to be drawn (finger down)
+                var path = new SKPath();
+                path.MoveTo(e.Location);
+                tempPath[e.Id] = path;
+            }
+            else if (e.ActionType == SKTouchAction.Moved) {
+                //While the line is being drawn (finger moving)
+                if (e.InContact)
+                {
+                    tempPath[e.Id].LineTo(e.Location);
+                }
+            }
+            else if (e.ActionType == SKTouchAction.Released) {
+                //When the line ends (finger up)
+                paths.Add(tempPath[e.Id]);
+                tempPath.Remove(e.Id);
+            }
+
+            //Once the event is handled, mark it as such and refresh the UI.
+            e.Handled = true;
+            ((SKCanvasView)sender).InvalidateSurface();
+        }
 	}
 }
