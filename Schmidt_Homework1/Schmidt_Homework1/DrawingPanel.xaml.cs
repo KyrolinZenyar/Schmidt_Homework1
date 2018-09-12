621cc323dafd940d2acee2f918a62200194590fc
Daniel Schmidt <schmidt.152@wright.edu>
2018-09-11 22:59:11 -0400

    Added buttons + clear functionality
    Added Color Picker, Clear, and Save buttons, along with Clear
    functionality.

--- Schmidt_Homework1/Schmidt_Homework1/DrawingPanel.xaml.cs
+++ Schmidt_Homework1/Schmidt_Homework1/DrawingPanel.xaml.cs
@@ -14,7 +14,7 @@ namespace Schmidt_Homework1
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DrawingPanel : ContentPage
 	{
-        private Dictionary<long, SKPath> tempPath = new Dictionary<long, SKPath>();
+        private Dictionary<long, SKPath> tempPaths = new Dictionary<long, SKPath>();
         private List<SKPath> paths = new List<SKPath>();
         private SKColor color = new SKColor(0, 0, 0);
         private int strokeWidth = 3;
@@ -28,7 +28,7 @@ namespace Schmidt_Homework1
         {
             var paintSurface = e.Surface;
             var paintCanvas = paintSurface.Canvas;
-            //Still need to add clear button and save button
+            
             paintCanvas.Clear(SKColors.White);
 
             //Define SKPaint here (need to make colorpicker and strokeWidth picker)
@@ -41,7 +41,7 @@ namespace Schmidt_Homework1
             };
 
             //As new paths (temporary or permanent) get created, draw them on the canvas
-            foreach (var path in tempPath)
+            foreach (var path in tempPaths)
             {
                 paintCanvas.DrawPath(path.Value, touchPathStroke);
             }
@@ -60,24 +60,48 @@ namespace Schmidt_Homework1
                 //When a line begins to be drawn (finger down)
                 var path = new SKPath();
                 path.MoveTo(e.Location);
-                tempPath[e.Id] = path;
+                tempPaths[e.Id] = path;
             }
             else if (e.ActionType == SKTouchAction.Moved) {
                 //While the line is being drawn (finger moving)
                 if (e.InContact)
                 {
-                    tempPath[e.Id].LineTo(e.Location);
+                    tempPaths[e.Id].LineTo(e.Location);
                 }
             }
             else if (e.ActionType == SKTouchAction.Released) {
                 //When the line ends (finger up)
-                paths.Add(tempPath[e.Id]);
-                tempPath.Remove(e.Id);
+                paths.Add(tempPaths[e.Id]);
+                tempPaths.Remove(e.Id);
             }
 
             //Once the event is handled, mark it as such and refresh the UI.
             e.Handled = true;
             ((SKCanvasView)sender).InvalidateSurface();
         }
-	}
+
+        //NEED TO ADD COLOR PICKER
+        private void OnColorPick(object sender, EventArgs e)
+        {
+            PickColor.Text = "Clicked!";
+        }
+
+        /***
+         * Method OnClear: Called on click of Clear button on Drawing Panel Tab
+         * Clears all drawn paths from drawing canvas.
+         **/
+        private void OnClear(object sender, EventArgs e)
+        {
+            //Clear list of drawn paths and refresh UI
+            paths.Clear();
+            Canvas.InvalidateSurface();
+        }
+
+        //NEED TO ADD SAVE FUNCTIONALITY
+        private void OnSave(object sender, EventArgs e)
+        {
+            Save.Text = "Clicked!";
+        }
+
+    }
 }
